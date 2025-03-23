3b063bdaeffa9275e6db3864972fd4828b1aa38c
Robbin-25 <robbin.m.gilster@gmail.com>
2025-03-23 16:43:23 +0100

    Update BEST_2.js
    Signed-off-by: Robbin-25 <robbin.m.gilster@gmail.com>

--- BEST_2.js
+++ BEST_2.js
@@ -1,4 +1,34 @@
+// Navbar -----------------------------------------------------------------------------------------------------
+function toggleMenu() {
+    document.querySelector(".nav-links").classList.toggle("active");
+    document.querySelector(".nav-hamburger").classList.toggle("active");
+}
 
+function toggleMenu() {
+    const navLinks = document.querySelector('.nav-links');
+    const hamburger = document.querySelector('.nav-hamburger');
+    const overlaynav = document.getElementById('overlay-nav');
+  
+    navLinks.classList.toggle('active');
+    hamburger.classList.toggle('active');
+  
+    if (navLinks.classList.contains('active')) {
+        overlaynav.style.display = 'block';
+    } else {
+        overlaynav.style.display = 'none';
+    }
+  }
+
+window.addEventListener("scroll", function () {
+    let navbar = document.querySelector(".navbar");
+    if (window.scrollY > 150) {
+      navbar.classList.add("scrolled");
+    } else {
+      navbar.classList.remove("scrolled");
+    }
+});
+
+// Hero -----------------------------------------------------------------------------------------------------
 document.addEventListener("DOMContentLoaded", function () {
     document.querySelector(".hero").classList.add("visible");
 });
