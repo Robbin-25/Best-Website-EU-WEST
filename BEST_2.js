a5da0c7ebfd5a6a9b09dd68e82095c1c7cf2b19c
Redcaus123 <enesschule2008@gmail.com>
2025-03-28 12:02:54 +0100

    Update BEST_2.js
    Signed-off-by: Redcaus123 <enesschule2008@gmail.com>

--- BEST_2.js
+++ BEST_2.js
@@ -50,3 +50,25 @@ document.addEventListener("DOMContentLoaded", function () {
         observer.observe(content);
     });
 });
+// Service -----------------------------------------------------------------------------------------------------
+
+const cards = document.querySelectorAll('.service-card');
+
+        const observerOptions = {
+            root: null,
+            rootMargin: '0px',
+            threshold: 0.1
+        };
+
+        const observer = new IntersectionObserver((entries, observer) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add('visible');
+                    observer.unobserve(entry.target);
+                }
+            });
+        }, observerOptions);
+
+        cards.forEach(card => {
+            observer.observe(card);
+        });
