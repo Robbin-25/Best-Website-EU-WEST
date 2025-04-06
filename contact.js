590adcf2b94e7a627a83730b5bf669831320445f
Redcaus123 <enesschule2008@gmail.com>
2025-04-06 19:34:31 +0200

    Update contact.js
    Signed-off-by: Redcaus123 <enesschule2008@gmail.com>

--- contact.js
+++ contact.js
@@ -25,3 +25,32 @@
             this.style.height = 'auto';
             this.style.height = (this.scrollHeight) + 'px';
         });
+
+function toggleMenu() {
+    document.querySelector(".nav-links").classList.toggle("active");
+    document.querySelector(".nav-hamburger").classList.toggle("active");
+}
+
+function toggleMenu() {
+    const navLinks = document.querySelector('.nav-links');
+    const hamburger = document.querySelector('.nav-hamburger');
+    const overlaynav = document.getElementById('overlay-nav');
+  
+    navLinks.classList.toggle('active');
+    hamburger.classList.toggle('active');
+  
+    if (navLinks.classList.contains('active')) {
+        overlaynav.style.display = 'block';
+    } else {
+        overlaynav.style.display = 'none';
+    }
+  }
+
+window.addEventListener("scroll", function () {
+    let navbar = document.querySelector(".navbar");
+    if (window.scrollY > 150) {
+      navbar.classList.add("scrolled");
+    } else {
+      navbar.classList.remove("scrolled");
+    }
+});
